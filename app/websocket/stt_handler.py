94e2e26a7ab51eb79fbe921f7f885741eeb0370b
hyojin425 <gywls5184@gmail.com>
2025-05-22 14:23:04 +0900

    fix: 웹 소켓 영상으로 받아서 stt 추출

--- app/websocket/stt_handler.py
+++ app/websocket/stt_handler.py
@@ -1,6 +1,7 @@
 import os
 import json
 import tempfile
+import subprocess
 
 from fastapi import WebSocket, WebSocketDisconnect
 
@@ -15,20 +16,41 @@ async def websocket_stt(websocket: WebSocket, intreviewId: int):
             audio_data = await websocket.receive_bytes()
 
             # 임시 파일로 저장
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as temp_file:
                 temp_file.write(audio_data)
-                temp_path = temp_file.name
+                webm_path = temp_file.name
 
             try:
+                # .wav로 파일 변환
+                wav_path = convert_webm_to_wav(webm_path)
+
                 # STT 처리
-                result = json.loads(SttService().req_upload(temp_path, completion='sync').text)
+                result = json.loads(SttService().req_upload(wav_path, completion='sync').text)
                 segments = result.get("segments", [])
+
                 if segments:
                     sentence = segments[0].get("text")
                     await websocket.send_text(sentence)
+
             finally:
                 # 분석 후 파일 삭제
-                os.remove(temp_path)
+                os.remove(webm_path)
+                if os.path.exists(wav_path):
+                    os.remove(wav_path)
 
     except WebSocketDisconnect:
         print("WebSocket 연결 종료")
+
+def convert_webm_to_wav(webm_path: str) -> str:
+    wav_path = webm_path.replace('.webm', '.wav')
+    command = [
+        "ffmpeg",
+        "-y",  # 기존 파일 덮어쓰기
+        "-i", webm_path,
+        "-ar", "16000",       # 샘플링 레이트 16kHz
+        "-ac", "1",           # 모노 채널
+        "-c:a", "pcm_s16le",  # WAV 기본 인코딩
+        wav_path
+    ]
+    subprocess.run(command, check=True)
+    return wav_path
