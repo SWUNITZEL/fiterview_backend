8abf657e2a4d4eb86ee56ce3753da29b9c4c5afb
jung <dnjswnddkgg@naver.com>
2025-05-27 15:40:26 +0900

    Refactor:면접 조합 생성 API 리팩토링

---
+++ app/repository/question_repository.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import List, Optional
+from bson import ObjectId
+from app.core.database import database
+
+question_collection = database['questions']
+
+class QuestionRepository:
+    def __init__(self):
+        self.collection = question_collection
+
+    async def save_questions(self, persona: str, major: str, questions: List[str]) -> List[str]:
+        docs = [{
+            "persona": persona,
+            "major": major,
+            "question": q,
+            "created_at": datetime.utcnow()
+        } for q in questions]
+
+        result = await self.collection.insert_many(docs)
+        return [str(_id) for _id in result.inserted_ids]
+
+    async def get_question_by_id(self, question_id: str) -> Optional[dict]:
+        try:
+            return await self.collection.find_one({"_id": ObjectId(question_id)})
+        except Exception:
+            return None
+
+
