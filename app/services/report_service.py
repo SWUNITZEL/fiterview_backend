7c558a13f7900c0abe1e3043a5a8b5bc0ce1c5fb
jung <dnjswnddkgg@naver.com>
2025-07-31 22:24:03 +0900

    feat:answer_id

--- app/services/report_service.py
+++ app/services/report_service.py
@@ -220,6 +220,8 @@ Step 3. 개선된 답변:
 
                 print("최종 video_paths:", video_paths)
                 report_items.append(QuestionReport(
+                    questionId=str(question_doc["_id"]),
+                    answerId=str(answer["_id"]),
                     question=question,
                     intent=intent,
                     answerText=answer_text,
